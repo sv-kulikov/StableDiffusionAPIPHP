299f636b7c8ffe706feb074f95f3d4ff86e57c2a
svyatoslavkulikov <svyatoslav_kulikov@epam.com>
2023-05-24 21:10:24 +0300

    Version 0.1 :).

--- class/StableDiffusion.php
+++ class/StableDiffusion.php
@@ -20,20 +20,20 @@ class StableDiffusion
         $totalPayloads = count($this->payloads);
 
         for ($i = 0; $i < $totalPayloads; $i++) {
-            echo "+++ Executing task " . ($i + 1) . " of " . $totalPayloads . " ...\n";
+            echo "→ Executing task " . ($i + 1) . " of " . $totalPayloads . " ...\n";
 
             for ($j = 0; $j < $this->payloads[$i]['iterations']; $j++) {
-                echo "+ Iteration " . ($j + 1) . " of " . $this->payloads[$i]['iterations'] . " ...\n";
-                echo "  Requesting " . $this->payloads[$i]['data']['batch_size'] . " images ...\n";
+                echo "┌ Iteration " . ($j + 1) . " of " . $this->payloads[$i]['iterations'] . " ...\n";
+                echo "│ Requesting " . $this->payloads[$i]['data']['batch_size'] . " images ...\n";
 
                 $payloadJSON = json_encode($this->payloads[$i]['data']);
                 $data = $this->curl->run($payloadJSON);
                 $dataDecoded = json_decode($data);
-                echo "  Got " . count($dataDecoded->images) . " images, saving to [" . $this->payloads[$i]['images_dir'] . "]... \n";
-                echo "  ";
+                echo "│ Got " . count($dataDecoded->images) . " images, saving to [" . $this->payloads[$i]['images_dir'] . "]... \n";
+                echo "│ ";
                 $this->image->saveImages($this->payloads[$i]['images_dir'], $this->payloads[$i]['data']['prompt'], $dataDecoded->images);
                 echo "\n";
-                echo "- Iteration " . ($j + 1) . " of " . $this->payloads[$i]['iterations'] . " done.\n";
+                echo "└ Iteration " . ($j + 1) . " of " . $this->payloads[$i]['iterations'] . " done.\n";
             }
         }
     }
