97191149d46fe09232d518669434754a49bb8260
svyatoslavkulikov <svyatoslav_kulikov@epam.com>
2023-05-28 18:38:06 +0300

    Version 0.15.

--- class/StableDiffusion.php
+++ class/StableDiffusion.php
@@ -17,7 +17,15 @@ class StableDiffusion
 
     public function run(): void
     {
+        $startTimeGlobal = microtime(true);
+
         $totalPayloads = count($this->payloads);
+        $imagesCreatedGlobal = 0;
+        $imagesToCreateGlobal = 0;
+
+        for ($i = 0; $i < $totalPayloads; $i++) {
+            $imagesToCreateGlobal += $this->payloads[$i]['data']['batch_size'];
+        }
 
         for ($i = 0; $i < $totalPayloads; $i++) {
             echo "→ Executing task " . ($i + 1) . " of " . $totalPayloads . " ...\n";
@@ -37,14 +45,29 @@ class StableDiffusion
                 $endTime = microtime(true);
 
                 $deltaTime = $endTime - $startTime;
+                $deltaTimeGlobal = $endTime - $startTimeGlobal;
 
-                $imagesPerSecond = round($this->payloads[$i]['data']['batch_size'] / $deltaTime, 2);
-                $secondsPerImage = round($deltaTime / $this->payloads[$i]['data']['batch_size'], 2);
+                $imagesCreated = count($dataDecoded->images);
+                $imagesCreatedGlobal += $imagesCreated;
+                $imagesPerSecond = round($imagesCreated / $deltaTime, 2);
+                $secondsPerImage = round($deltaTime / $imagesCreated, 2);
+                $imagesPerSecondGlobal = round($imagesCreatedGlobal / $deltaTimeGlobal, 2);
+                $secondsPerImageGlobal = round($deltaTimeGlobal / $imagesCreatedGlobal, 2);
 
                 if ($imagesPerSecond >= 1) {
                     $performanceMsg = "Images per second = " . $imagesPerSecond . ".";
                 } else {
                     $performanceMsg = "Seconds per image = " . $secondsPerImage . ".";
+                }
+
+                if ($imagesPerSecondGlobal >= 1) {
+                    $estimatedTimeLeft = round(($imagesToCreateGlobal - $imagesCreatedGlobal) / $imagesPerSecondGlobal);
+                    $performanceMsg .= " Images per second global = " . $imagesPerSecondGlobal . ".";
+                    $performanceMsg .= " ETA = " . sprintf('%02d:%02d:%02d', (int)($estimatedTimeLeft / 3600), (int)($estimatedTimeLeft / 60 % 60), (int)($estimatedTimeLeft % 60)) . ".";
+                } else {
+                    $estimatedTimeLeft = round($secondsPerImageGlobal * ($imagesToCreateGlobal - $imagesCreatedGlobal));
+                    $performanceMsg .= " Seconds per image global = " . $secondsPerImageGlobal . ".";
+                    $performanceMsg .= " ETA = " . sprintf('%02d:%02d:%02d', (int)($estimatedTimeLeft / 3600), (int)($estimatedTimeLeft / 60 % 60), (int)($estimatedTimeLeft % 60)) . ".";
                 }
 
                 echo "\n";
